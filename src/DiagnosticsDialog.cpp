a507d82d49c42379d1a58349068fbe5c6cced313
Raul Metsma <raul@metsma.ee>
2010-12-20 19:26:59 +0000

    move files around
    git-svn-id: https://svn.eesti.ee/projektid/idkaart/qesteidutil/trunk@2198 c45a0044-e149-41c6-9d26-0409f48354e9

--- src/DiagnosticsDialog.cpp
+++ src/DiagnosticsDialog.cpp
@@ -19,172 +19,82 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
-
 #include "DiagnosticsDialog.h"
 
+#include <smartcardpp/common.h>
+#include <smartcardpp/PCSCManager.h>
+#include <smartcardpp/esteid/EstEidCard.h>
+
+#include <QDesktopServices>
 #include <QFile>
+#include <QFileDialog>
+#include <QMessageBox>
 #include <QTextStream>
 
-#if defined(Q_OS_LINUX)
-#include <QProcess>
-#elif defined(Q_OS_MAC)
-#include <Carbon/Carbon.h>
-#include <QProcess>
-#endif
-
-DiagnosticsDialog::DiagnosticsDialog( QWidget *parent )
-:	QDialog( parent )
+QString DiagnosticsDialog::getReaderInfo()
 {
-	setupUi( this );
-	setAttribute( Qt::WA_DeleteOnClose, true );
+	QString d;
+	QTextStream s( &d );
 
-	QString info;
-	QTextStream s( &info );
-
-	s << "<b>" << tr("Locale:") << "</b> ";
-	QString locale = QLocale::c().name();
-	s << (locale == "C" ? "en_us" : locale) << "<br /><br />";
-
-#if defined(Q_OS_LINUX)
-	QString package = getPackageVersion( QStringList() << "estonianidcard", false );
-	QString utility = getPackageVersion( QStringList() << "qesteidutil", false );
-	if ( !package.isEmpty() )
-		s << "<b>" << tr("ID-card package version:") << "</b> " << package << "<br />";
-	if ( !utility.isEmpty() )
-		s << "<b>" << tr("ID-card utility version:") << "</b> " << utility << "<br />";
-#else
-	s << "<b>" << tr("ID-card utility version:") << "</b> " << QCoreApplication::applicationVersion() << "<br />";
+	QHash<QString,QString> readers;
+	QString reader;
+	PCSCManager *m = 0;
+	try {
+		m = new PCSCManager();
+	} catch( const std::runtime_error & ) {
+		readers[reader] = tr("No readers found");
+	}
+	if ( m )
+	{
+		try {
+			int readersCount = m->getReaderCount( true );
+			for( int i = 0; i < readersCount; i++ )
+			{
+				reader = QString::fromStdString( m->getReaderName( i ) );
+				if ( !QString::fromStdString( m->getReaderState( i ) ).contains( "EMPTY" ) )
+				{
+					EstEidCard card( *m, i );
+					QString cardId = QString::fromStdString( card.readCardID() ); 
+					readers[reader] = tr( "ID - %1<br />ATR - %2" )
+						.arg( cardId )
+						.arg( QString::fromStdString( m->getATRHex( i ) ).toUpper() );
+#if defined(Q_OS_WIN32)
+					certInfo += checkCert( card.getAuthCert(), card.getSignCert(), cardId );
 #endif
+				} else
+					readers[reader] = "";
+			}
+		} catch( const std::runtime_error &e ) {
+			readers[reader] = tr("Error reading card data:") + e.what();
+		}
+	}
+	if ( m )
+		delete m;
 
-	s << "<b>" << tr("OS:") << "</b> ";
-#if defined(Q_OS_LINUX)
-	QProcess p;
-	p.start( "lsb_release", QStringList() << "-s" << "-d" );
-	p.waitForReadyRead();
-	s << p.readAll();
-#elif defined(Q_OS_MAC)
-	SInt32 major, minor, bugfix;
-	
-	if( Gestalt(gestaltSystemVersionMajor, &major) == noErr &&
-			Gestalt(gestaltSystemVersionMinor, &minor) == noErr &&
-			Gestalt(gestaltSystemVersionBugFix, &bugfix) == noErr )
-		s << "Mac OS " << major << "." << minor << "." << bugfix;
+	for( QHash<QString,QString>::const_iterator i = readers.constBegin();
+		i != readers.constEnd(); ++i )
+	{
+		if ( !i.key().isEmpty() )
+			s << "* " << i.key();
+		if( !i.value().isEmpty() )
+			s << "<p style='margin-left: 10px; margin-top: 0px; margin-bottom: 0px; margin-right: 0px;'>" << i.value() << "</p>";
+		else
+			s << "<br />";
+	}
+
+	return d;
+}
+
+void DiagnosticsDialog::save()
+{
+	QString filename = QFileDialog::getSaveFileName( this, tr("Save as"), QString( "%1%2qesteidutil_diagnostics.txt" )
+		.arg( QDesktopServices::storageLocation( QDesktopServices::DocumentsLocation ) ).arg( QDir::separator() ),
+		tr("Text files (*.txt)") );
+	if( filename.isEmpty() )
+		return;
+	QFile f( filename );
+	if( f.open( QIODevice::WriteOnly ) )
+		QTextStream( &f ) << diagnosticsText->toPlainText();
 	else
-		s << "Mac OS 10.3";
-#endif
-
-	s << " (" << QSysInfo::WordSize << ")<br />";
-#if defined(Q_OS_LINUX)
-	s << "<b>" << tr("CPU:") << "</b> " << getProcessor() << "<br /><br />";
-#endif
-
-	s << "<b>" << tr("Library paths:") << "</b> " << QCoreApplication::libraryPaths().join( ";" ) << "<br />";
-
-	s << "<b>" << tr("Libraries") << ":</b><br />";
-#if defined(Q_OS_MAC)
-	QProcess p;
-	p.start( "/Library/OpenSC/bin/opensc-tool", QStringList() << "-i" );
-	p.waitForReadyRead();
-	s << p.readAll() << "<br />";
-#elif defined(Q_OS_LINUX)
-	s << getPackageVersion( QStringList() << "openssl" << "libpcsclite1" << "opensc" );
-#endif
-	s << "QT (" << qVersion() << ")<br />" << "<br />";
-
-	s << "<b>" << tr("PCSC service status: ") << "</b>" << " " << (isPCSCRunning() ? tr("Running") : tr("Not running")) << "<br /><br />";
-
-	s << "<b>" << tr("Card readers") << ":</b><br />" << getReaderInfo() << "<br />";
-
-	QString browsers;
-#if defined(Q_OS_LINUX)
-	browsers = getPackageVersion( QStringList() << "chromium-browser" << "firefox" << "MozillaFirefox");
-#elif defined(Q_OS_MAC)
-	browsers = getPackageVersion( QStringList() << "Google Chrome" << "Firefox" << "Safari" );
-#endif
-	if ( !browsers.isEmpty() )
-		s << "<b>" << tr("Browsers:") << "</b><br />" << browsers << "<br /><br />";
-
-	diagnosticsText->setHtml( info );
-}
-
-QString DiagnosticsDialog::getPackageVersion( const QStringList &list, bool returnPackageName ) const
-{
-	QString ret;
-#if defined(Q_OS_LINUX)
-	QStringList params;
-	QProcess p;
-	p.start( "which", QStringList() << "dpkg-query" );
-	p.waitForReadyRead();
-	QByteArray cmd = p.readAll();
-	if ( cmd.isEmpty() )
-	{
-		p.start( "which", QStringList() << "rpm" );
-		p.waitForReadyRead();
-		cmd = p.readAll();
-		if ( cmd.isEmpty() )
-			return ret;
-		cmd = "rpm";
-		params << "-q" << "--qf" << "%{VERSION}";
-	} else {
-		cmd = "dpkg-query";
-		params << "-W" << "-f=${Version}";
-	}
-	p.close();
-
-	Q_FOREACH( QString package, list )
-	{
-		p.start( cmd, QStringList() << params << package );
-		p.waitForReadyRead();
-		QByteArray result = p.readAll();
-		if ( !result.isEmpty() )
-		{
-			if ( returnPackageName )
-				ret += package + " ";
-			ret += result + "<BR />";
-		}
-		p.close();
-	}
-#elif defined(Q_OS_MAC)
-	QProcess p;
-	Q_FOREACH( QString package, list )
-	{
-		QString app = "/Applications/" + package + ".app/Contents/Info";
-		if ( !QFile::exists( app + ".plist") )
-			continue;
-		p.start( "defaults", QStringList() << "read" << app << "CFBundleShortVersionString" );
-		p.waitForReadyRead();
-		QByteArray result = p.readAll();
-		if ( !result.isEmpty() )
-		{
-			if ( returnPackageName )
-				ret += package + " ";
-			ret += result + "<BR />";
-		}
-		p.close();
-	}
-#endif
-
-	return ret;
-}
-
-QString DiagnosticsDialog::getProcessor() const
-{
-	QProcess p;
-	p.start( "sh -c \"cat /proc/cpuinfo | grep -m 1 model\\ name\"" );
-	p.waitForReadyRead();
-	return p.readAll();
-}
-
-bool DiagnosticsDialog::isPCSCRunning() const
-{
-	QProcess p;
-#if defined(Q_OS_LINUX)
-	p.start( "pidof", QStringList() << "pcscd" );
-#elif defined(Q_OS_MAC)
-	p.start( "sh -c \"ps ax | grep -v grep | grep pcscd\"" );
-#else
-	return true;
-#endif
-	p.waitForFinished();
-	return !p.readAll().trimmed().isEmpty();
+		QMessageBox::warning( this, tr("Error occured"), tr("Failed write to file!") );
 }
---
+++ src/DiagnosticsDialog_unix.cpp
@@ -0,0 +1,190 @@
+/*
+ * QEstEidUtil
+ *
+ * Copyright (C) 2009,2010 Jargo Kõster <jargo@innovaatik.ee>
+ * Copyright (C) 2009,2010 Raul Metsma <raul@innovaatik.ee>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include "DiagnosticsDialog.h"
+
+#include <QFile>
+#include <QTextStream>
+
+#if defined(Q_OS_LINUX)
+#include <QProcess>
+#elif defined(Q_OS_MAC)
+#include <Carbon/Carbon.h>
+#include <QProcess>
+#endif
+
+DiagnosticsDialog::DiagnosticsDialog( QWidget *parent )
+:	QDialog( parent )
+{
+	setupUi( this );
+	setAttribute( Qt::WA_DeleteOnClose, true );
+
+	QString info;
+	QTextStream s( &info );
+
+	s << "<b>" << tr("Locale:") << "</b> ";
+	QString locale = QLocale::c().name();
+	s << (locale == "C" ? "en_us" : locale) << "<br /><br />";
+
+#if defined(Q_OS_LINUX)
+	QString package = getPackageVersion( QStringList() << "estonianidcard", false );
+	QString utility = getPackageVersion( QStringList() << "qesteidutil", false );
+	if ( !package.isEmpty() )
+		s << "<b>" << tr("ID-card package version:") << "</b> " << package << "<br />";
+	if ( !utility.isEmpty() )
+		s << "<b>" << tr("ID-card utility version:") << "</b> " << utility << "<br />";
+#else
+	s << "<b>" << tr("ID-card utility version:") << "</b> " << QCoreApplication::applicationVersion() << "<br />";
+#endif
+
+	s << "<b>" << tr("OS:") << "</b> ";
+#if defined(Q_OS_LINUX)
+	QProcess p;
+	p.start( "lsb_release", QStringList() << "-s" << "-d" );
+	p.waitForReadyRead();
+	s << p.readAll();
+#elif defined(Q_OS_MAC)
+	SInt32 major, minor, bugfix;
+	
+	if( Gestalt(gestaltSystemVersionMajor, &major) == noErr &&
+			Gestalt(gestaltSystemVersionMinor, &minor) == noErr &&
+			Gestalt(gestaltSystemVersionBugFix, &bugfix) == noErr )
+		s << "Mac OS " << major << "." << minor << "." << bugfix;
+	else
+		s << "Mac OS 10.3";
+#endif
+
+	s << " (" << QSysInfo::WordSize << ")<br />";
+#if defined(Q_OS_LINUX)
+	s << "<b>" << tr("CPU:") << "</b> " << getProcessor() << "<br /><br />";
+#endif
+
+	s << "<b>" << tr("Library paths:") << "</b> " << QCoreApplication::libraryPaths().join( ";" ) << "<br />";
+
+	s << "<b>" << tr("Libraries") << ":</b><br />";
+#if defined(Q_OS_MAC)
+	QProcess p;
+	p.start( "/Library/OpenSC/bin/opensc-tool", QStringList() << "-i" );
+	p.waitForReadyRead();
+	s << p.readAll() << "<br />";
+#elif defined(Q_OS_LINUX)
+	s << getPackageVersion( QStringList() << "openssl" << "libpcsclite1" << "opensc" );
+#endif
+	s << "QT (" << qVersion() << ")<br />" << "<br />";
+
+	s << "<b>" << tr("PCSC service status: ") << "</b>" << " " << (isPCSCRunning() ? tr("Running") : tr("Not running")) << "<br /><br />";
+
+	s << "<b>" << tr("Card readers") << ":</b><br />" << getReaderInfo() << "<br />";
+
+	QString browsers;
+#if defined(Q_OS_LINUX)
+	browsers = getPackageVersion( QStringList() << "chromium-browser" << "firefox" << "MozillaFirefox");
+#elif defined(Q_OS_MAC)
+	browsers = getPackageVersion( QStringList() << "Google Chrome" << "Firefox" << "Safari" );
+#endif
+	if ( !browsers.isEmpty() )
+		s << "<b>" << tr("Browsers:") << "</b><br />" << browsers << "<br /><br />";
+
+	diagnosticsText->setHtml( info );
+}
+
+QString DiagnosticsDialog::getPackageVersion( const QStringList &list, bool returnPackageName ) const
+{
+	QString ret;
+#if defined(Q_OS_LINUX)
+	QStringList params;
+	QProcess p;
+	p.start( "which", QStringList() << "dpkg-query" );
+	p.waitForReadyRead();
+	QByteArray cmd = p.readAll();
+	if ( cmd.isEmpty() )
+	{
+		p.start( "which", QStringList() << "rpm" );
+		p.waitForReadyRead();
+		cmd = p.readAll();
+		if ( cmd.isEmpty() )
+			return ret;
+		cmd = "rpm";
+		params << "-q" << "--qf" << "%{VERSION}";
+	} else {
+		cmd = "dpkg-query";
+		params << "-W" << "-f=${Version}";
+	}
+	p.close();
+
+	Q_FOREACH( QString package, list )
+	{
+		p.start( cmd, QStringList() << params << package );
+		p.waitForReadyRead();
+		QByteArray result = p.readAll();
+		if ( !result.isEmpty() )
+		{
+			if ( returnPackageName )
+				ret += package + " ";
+			ret += result + "<BR />";
+		}
+		p.close();
+	}
+#elif defined(Q_OS_MAC)
+	QProcess p;
+	Q_FOREACH( QString package, list )
+	{
+		QString app = "/Applications/" + package + ".app/Contents/Info";
+		if ( !QFile::exists( app + ".plist") )
+			continue;
+		p.start( "defaults", QStringList() << "read" << app << "CFBundleShortVersionString" );
+		p.waitForReadyRead();
+		QByteArray result = p.readAll();
+		if ( !result.isEmpty() )
+		{
+			if ( returnPackageName )
+				ret += package + " ";
+			ret += result + "<BR />";
+		}
+		p.close();
+	}
+#endif
+
+	return ret;
+}
+
+QString DiagnosticsDialog::getProcessor() const
+{
+	QProcess p;
+	p.start( "sh -c \"cat /proc/cpuinfo | grep -m 1 model\\ name\"" );
+	p.waitForReadyRead();
+	return p.readAll();
+}
+
+bool DiagnosticsDialog::isPCSCRunning() const
+{
+	QProcess p;
+#if defined(Q_OS_LINUX)
+	p.start( "pidof", QStringList() << "pcscd" );
+#elif defined(Q_OS_MAC)
+	p.start( "sh -c \"ps ax | grep -v grep | grep pcscd\"" );
+#else
+	return true;
+#endif
+	p.waitForFinished();
+	return !p.readAll().trimmed().isEmpty();
+}
