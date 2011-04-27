6bd75de135ef72d9becf10ceacd0e245c6a013ef
Raul Metsma <raul@metsma.ee>
2011-04-27 16:11:46 +0000

    fix build, include common resources
    Ticket: #8402
    
    
    git-svn-id: https://svn.eesti.ee/projektid/idkaart/qesteidutil/trunk@3599 c45a0044-e149-41c6-9d26-0409f48354e9

---
+++ common/resources.cmake
@@ -0,0 +1,9 @@
+set( COMMON ${CMAKE_SOURCE_DIR}/common )
+set( COMMON_RCS ${COMMON}/images/common_images.qrc )
+set( COMMON_TS
+	${COMMON}/translations/common_en.ts
+	${COMMON}/translations/common_et.ts
+	${COMMON}/translations/common_ru.ts
+	${COMMON}/translations/qt_et.ts
+	${COMMON}/translations/qt_ru.ts
+	)
