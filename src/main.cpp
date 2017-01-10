904ad292a4b8cf46afbf6cd5ea42b64a6cb9b10d
Raul Metsma <raul@metsma.ee>
2017-01-10 09:47:32 +0200

    Enable HiDPI on Windows (#18)
    IB-3752
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,10 @@
 
 int main(int argc, char *argv[])
 {
+#if QT_VERSION > QT_VERSION_CHECK(5, 6, 0)
+	QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+#endif
 	CliApplication cliApp( argc, argv, APP );
 	if( cliApp.isDiagnosticRun() )
 	{
